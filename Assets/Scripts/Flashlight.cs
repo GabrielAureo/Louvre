5639c3b8b6907b0269938c4e0c3c832acae90e2a
Gabriel Aureo <gabrielaureo@gmail.com>
2018-08-01 22:13:27 -0300

    Post processing setup and Dotween import

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Flashlight : MonoBehaviour {
+
+	Light source;
+
+	float intensity;
+	float range;
+
+	[SerializeField] float switchDuration = .5f;
+
+	void Awake(){
+		source = GetComponentInChildren<Light>(); 
+		intensity = source.intensity;
+		range = source.range;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			//source.DOIntensity(source.intensity == 0 ? intensity: 0 ,switchDuration).Play();
+			DOTween.To(()=>source.range, x=>source.range = x, source.range == 0 ? range: 0,switchDuration);
+			
+		}
+					
+	}
+}
