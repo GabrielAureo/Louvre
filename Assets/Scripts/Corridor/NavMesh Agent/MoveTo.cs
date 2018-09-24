065b08a2c55c4d052b76471ff000aad01be431ca
vband <vtor.barcellos7@gmail.com>
2018-09-23 22:39:57 -0300

    Implementada uma parte do level design da fase 1 usando tilemap

---
+++ Assets/Scripts/Corridor/NavMesh Agent/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    [SerializeField] private Transform goal;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.position);
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(goal.position);
+    }
+}
