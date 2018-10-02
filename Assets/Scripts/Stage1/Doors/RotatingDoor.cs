16fbba026dd39d13069eb1d976767b5ea896f5d0
vband <vtor.barcellos7@gmail.com>
2018-10-02 11:57:07 -0300

    Aprimorada imagem das placas

--- Assets/Scripts/Stage1/Doors/RotatingDoor.cs
+++ Assets/Scripts/Stage1/Doors/RotatingDoor.cs
@@ -16,14 +16,22 @@ public class RotatingDoor : Door
     public void Unlock()
     {
         StartCoroutine("OpenDoor");
+        OpenDoor();
     }
 
     private IEnumerator OpenDoor()
     {
-        while (transform.localRotation.eulerAngles.y < angleToRotate)
+        while (transform.rotation.eulerAngles.y < angleToRotate)
         {
             transform.RotateAround(hinge.position, Vector3.up, angleToRotate * Time.deltaTime * durationOfRotation);
             yield return new WaitForEndOfFrame();
         }
     }
+
+    /*private void OpenDoor()
+    {
+        //transform.RotateAround(hinge.position, transform.forward, angleToRotate);
+        Vector3 rot = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(rot.x, rot.y, angleToRotate);
+    }*/
 }
